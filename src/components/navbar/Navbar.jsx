c0494a5ebbc1f64754edcdcdd950a31ada496bb7
bobur1998brm <github1998brm>
2022-08-10 11:12:12 +0500

    bismillah

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import s from "./Navbar.module.css"
+
+function Navbar() {
+  return (
+    <div className={s.navbar}>Navbar</div>
+  )
+}
+
+export default Navbar
--- src/index.js
+++ src/index.js
@@ -1,14 +1,42 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {BrowserRouter} from "react-router-dom"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+import rootReducer from "./context/reducer"
+import { legacy_createStore as createStore } from "redux";
+import { Provider } from "react-redux"
+
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage' 
+import { PersistGate } from 'redux-persist/integration/react'
+
+const persistConfig = {
+  key: 'mustang',
+  storage,
+  blacklist: [],
+  whitelist: []
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+const store = createStore(persistedReducer)
+const persister = persistStore(store)
+
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <BrowserRouter>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persister}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
