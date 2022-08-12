f25413fb323f4a7118ad4b2c7e19952253593f50
bobur1998brm <github1998brm>
2022-08-12 10:51:56 +0500

    created router and components

---
+++ src/components/filter-product/FilterProduct.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import s from "./FilterProduct.module.css"
+
+function FilterProduct() {
+  return (
+    <div className={s.filter}>FilterProduct</div>
+  )
+}
+
+export default FilterProduct
