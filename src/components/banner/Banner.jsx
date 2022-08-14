8e4fae4122da4e0283cce242c190cfe6285ca801
JuravekJr <jurabek009developer@gmail.com>
2022-08-14 09:42:09 +0500

    Banner & Login - Frontend - Full HD

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -1,8 +1,47 @@
 import React from 'react'
+import img from '../../assets/banner.png'
+
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+import s from './Banner.module.css'
+
+import { Autoplay, Pagination } from "swiper";
 
 function Banner() {
   return (
-    <div>Banner</div>
+    <Swiper
+    centeredSlides={true}
+    autoplay={{
+      delay: 5000,
+      disableOnInteraction: false,
+    }}
+    loop={true}
+    pagination={{
+      clickable: true,
+    }}
+    modules={[Autoplay, Pagination]}
+    className={s.mySwiper}
+    >
+      <SwiperSlide className={s.swiper_slider}>
+        <img src={img} alt="Banner image 1." />
+      </SwiperSlide>
+      <SwiperSlide className={s.swiper_slider}>
+        <img src="https://images.theconversation.com/files/303798/original/file-20191126-112499-1j4wq8y.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=754&fit=clip" alt="Banner image 2." />
+      </SwiperSlide>
+      <SwiperSlide className={s.swiper_slider}>
+        <img src="https://cdn.shopify.com/s/files/1/0468/9441/files/luccamonkhoneyfloral.jpg?v=1627659977&width=2000" alt="Banner image 3." />
+      </SwiperSlide>
+      <SwiperSlide className={s.swiper_slider}>
+        <img src="https://biliboltv.uz/wp-content/uploads/2019/12/products.13333.1.b-1-2.jpg" alt="Banner image 4." />
+      </SwiperSlide>
+      <SwiperSlide className={s.swiper_slider}>
+        <img src="https://manofmany.com/wp-content/uploads/2015/03/Santoni-WIlson-Leather-Oxford.jpg" alt="Banner image 5." />
+      </SwiperSlide>
+    </Swiper>
   )
 }
 
--- src/router/login/Login.jsx
+++ src/router/login/Login.jsx
@@ -1,8 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
+import s from './Login.module.css'
+import { Link } from 'react-router-dom'
 
 function Login() {
+  const [user, setUser] = useState({
+    username: "",
+    password: ""
+  })
+
+  // console.log(user);
   return (
-    <div>Login</div>
+    <div className={s.login}>
+      <div className={s.card}>
+        <h1 className={s.title}>login</h1>
+        <div className={[s.modal, s.suc].join(' ')}>
+          <p className={s.modal_text}>Lorem, ipsum.</p>
+        </div>
+        <form className={s.form}>
+          <label htmlFor="inp_name" className={s.input_text}>Sizning usernamingiz:</label>
+          <input 
+          value={user.username}
+          onChange={({target}) => setUser({...user, username: target.value})}
+          type="text" 
+          id='inp_name' 
+          className={s.inp} 
+          />
+          <label htmlFor="inp_pw" className={s.input_text}>Sizning parolingiz:</label>
+          <input 
+          value={user.password}
+          onChange={({target}) => setUser({...user, password: target.value})}
+          type="password" 
+          id='inp_pw' 
+          className={s.inp} 
+          />
+          <button className={s.submit_btn}>Login</button>
+        </form>
+        <div className={s.border}></div>
+        <Link to='/' className={s.backhome_btn}>Asosiy sahifaga qaytish</Link>
+      </div>
+    </div>
   )
 }
 
