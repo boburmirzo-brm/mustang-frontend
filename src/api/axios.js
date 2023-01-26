9f862503f0db9bf022c0f0259794e1286efb23db
bobur1998brm <github1998brm>
2023-01-26 19:56:06 +0500

    api

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 const mainURL = axios.create({
     // baseURL: "http://localhost:8000/"
-    baseURL: "https://mustang-backend.herokuapp.com"
+    baseURL: "https://mustang-backend-ofhc-main-h4nwdglgda-wm.a.run.app/"
 })
 export default mainURL
