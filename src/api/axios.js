7f5af8fbcb852c026774be0021595b28df9270a2
boburmirzo-brm <boburmirzo.brm@gmail.com>
2023-05-23 22:35:14 +0500

    changed api url

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,7 @@
 import axios from "axios"
 const mainURL = axios.create({
     // baseURL: "http://localhost:8000/"
-    baseURL: "https://mustang-backend-ofhc-main-h4nwdglgda-wm.a.run.app/"
+    // baseURL: "https://mustang-backend-ofhc-main-h4nwdglgda-wm.a.run.app/"
+    baseURL: "https://mustang-backend.onrender.com/"
 })
 export default mainURL
